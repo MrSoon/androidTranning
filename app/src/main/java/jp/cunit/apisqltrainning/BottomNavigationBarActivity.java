90063791f3956303013206730d746d700e67dd0b
Son Do <sondv0411@gmail.com>
2021-12-18 16:24:38 +0700

    add bottomnavigationview

--- app/src/main/java/jp/cunit/apisqltrainning/BottomNavigationBarActivity.java
+++ app/src/main/java/jp/cunit/apisqltrainning/BottomNavigationBarActivity.java
@@ -1,32 +1,25 @@
 package jp.cunit.apisqltrainning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-import jp.cunit.apisqltrainning.adapter.ViewpageAdapter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TableLayout;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
-public class TabViewpage2FrapmentActivity extends AppCompatActivity {
-
-    private TabLayout tablayout;
-    private ViewPager2 viewpage;
+public class BottomNavigationBarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab_viewpage2_frapment);
-        tablayout = findViewById(R.id.tablayout);
-        viewpage = findViewById(R.id.viewpage);
+        setContentView(R.layout.activity_bottom_navigation_bar);
 
         //define bottom navigation
         BottomNavigationView bottom_navi = (BottomNavigationView)findViewById(R.id.bottom_navi);
-        bottom_navi.getMenu().findItem(R.id.menu_item_setting).setChecked(true);
+        bottom_navi.getMenu().findItem(R.id.menu_item_cart).setChecked(true);
         bottom_navi.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.menu_item_home:
@@ -45,23 +38,6 @@ public class TabViewpage2FrapmentActivity extends AppCompatActivity {
             return true;
         });
         //end
-
-        ViewpageAdapter viewpageAdapter = new ViewpageAdapter(this);
-        viewpage.setAdapter(viewpageAdapter);
-        new TabLayoutMediator(tablayout, viewpage, (tab, position) -> {
-            switch (position){
-                case 0:
-                    tab.setText("Home");
-                    break;
-                case 1:
-                    tab.setText("Info");
-                    break;
-                case 2:
-                    tab.setText("Detail");
-                    break;
-            }
-
-        }).attach();
 
     }
 }
--- app/src/main/java/jp/cunit/apisqltrainning/MainActivity.java
+++ app/src/main/java/jp/cunit/apisqltrainning/MainActivity.java
@@ -16,10 +16,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivity";
-    private Button btn_retrofit, btn_room, btn_download, btn_tabviewfrag;
+    private Button btn_retrofit, btn_room, btn_download, btn_tabviewfrag, btn_bottomnav;
 
 
     @Override
@@ -30,7 +32,29 @@ public class MainActivity extends AppCompatActivity {
         btn_room = findViewById(R.id.btn_room);
         btn_download = findViewById(R.id.btn_download);
         btn_tabviewfrag = findViewById(R.id.btn_tabviewfrag);
+        btn_bottomnav = findViewById(R.id.btn_bottomnav);
 
+        //define bottom navigation
+        BottomNavigationView bottom_navi = (BottomNavigationView)findViewById(R.id.bottom_navi);
+        bottom_navi.getMenu().findItem(R.id.menu_item_home).setChecked(true);
+        bottom_navi.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.menu_item_home:
+                    Intent it = new Intent(this, MainActivity.class);
+                    startActivity(it);
+                    break;
+                case R.id.menu_item_setting:
+                    Intent it1 = new Intent(this, TabViewpage2FrapmentActivity.class);
+                    startActivity(it1);
+                    break;
+                case R.id.menu_item_cart:
+                    Intent it2 = new Intent(this, BottomNavigationBarActivity.class);
+                    startActivity(it2);
+                    break;
+            }
+            return true;
+        });
+        //end
 
         //
         btn_retrofit.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +86,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent itRoom = new Intent(MainActivity.this, TabViewpage2FrapmentActivity.class);
+                startActivity(itRoom);
+            }
+        });
+
+        btn_bottomnav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent itRoom = new Intent(MainActivity.this, BottomNavigationBarActivity.class);
                 startActivity(itRoom);
             }
         });
