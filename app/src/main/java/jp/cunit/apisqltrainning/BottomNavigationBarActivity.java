a323438d917ba5f8791d97f579ac0e81add48247
MrSoon <sondo0411@gmail.com>
2021-12-19 10:48:42 +0700

    change bottomnavigation with fragment

--- app/src/main/java/jp/cunit/apisqltrainning/BottomNavigationBarActivity.java
+++ app/src/main/java/jp/cunit/apisqltrainning/BottomNavigationBarActivity.java
@@ -2,6 +2,8 @@ package jp.cunit.apisqltrainning;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import jp.cunit.apisqltrainning.adapter.ViewpageAdapter;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,10 +14,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNavigationBarActivity extends AppCompatActivity {
 
+    private ViewPager2 viewpage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation_bar);
+        viewpage = findViewById(R.id.viewpage2);
+
+        ViewpageAdapter viewpageAdapter = new ViewpageAdapter(this);
+        viewpage.setAdapter(viewpageAdapter);
 
         //define bottom navigation
         BottomNavigationView bottom_navi = (BottomNavigationView)findViewById(R.id.bottom_navi);
@@ -23,21 +30,37 @@ public class BottomNavigationBarActivity extends AppCompatActivity {
         bottom_navi.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.menu_item_home:
-                    Intent it = new Intent(this, MainActivity.class);
-                    startActivity(it);
+                    viewpage.setCurrentItem(0);
                     break;
                 case R.id.menu_item_setting:
-                    Intent it1 = new Intent(this, TabViewpage2FrapmentActivity.class);
-                    startActivity(it1);
+                    viewpage.setCurrentItem(1);
                     break;
                 case R.id.menu_item_cart:
-                    Intent it2 = new Intent(this, BottomNavigationBarActivity.class);
-                    startActivity(it2);
+                    viewpage.setCurrentItem(2);
                     break;
             }
             return true;
         });
         //end
+
+        //set checked of item when change fragment
+        viewpage.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position){
+                    case 0:
+                        bottom_navi.getMenu().findItem(R.id.menu_item_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottom_navi.getMenu().findItem(R.id.menu_item_setting).setChecked(true);
+                        break;
+                    case 2:
+                        bottom_navi.getMenu().findItem(R.id.menu_item_cart).setChecked(true);
+                        break;
+                }
+            }
+        });
 
     }
 }
